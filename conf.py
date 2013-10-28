35ce76e545294adef87e5575f5be1c98954014a8
windkeepblow <shaoyf2011@gmail.com>
2013-10-28 15:38:15 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -5,7 +5,7 @@ database = "imdbdb" #The name of the database(mongodb)
 waitTime = 60 #When reach the request limit, how many seconds will the program wait for
 
 ##Parameters for debug
-startID = 2675 #From what sequence number of movie should the crawler begin crawling
-ratingStartID = 60 #From what sequence number of comment(rating) to begin crawling
-movieID = "0450345" #From what movieID to crawl, -1 is to disable the movieID.(type: String)
+startID = 1 #From what sequence number of movie should the crawler begin crawling
+ratingStartID = 0 #From what sequence number of comment(rating) to begin crawling
+movieID = "-1" #From what movieID to crawl, -1 is to disable the movieID.(type: String)
 
